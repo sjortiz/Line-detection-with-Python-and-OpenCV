3fad2a1592369a9e7614b36829038da643a1cc5e
ShailynOrtiz <sortiz@intellisys.com.do>
2018-12-30 23:36:04 -0400

    streamming lines, making the lines two instead of chunks

--- lanes.py
+++ lanes.py
@@ -2,6 +2,48 @@ import cv2
 import numpy as np
 
 
+def make_coordinates(image, line_parameters):
+
+    slope, intercept = line_parameters
+
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+
+    return np.array([x1, y1, x2, y2])
+
+
+def average_slope_intercept(image, lines):
+
+    left_fit = []
+    right_fit = []
+
+    for line in lines:
+
+        x1, y1, x2, y2 = line.reshape(4)
+
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+
+        slope = parameters[0]
+        intercept = parameters[1]
+
+        if slope < 0:
+            left_fit.append((slope, intercept))
+
+        else:
+            right_fit.append((slope, intercept))
+
+    left_fit_avg = np.average(left_fit, axis=0)
+    right_fit_avg = np.average(right_fit, axis=0)
+
+
+    left_line = make_coordinates(image, left_fit_avg)
+    right_line = make_coordinates(image, right_fit_avg)
+
+    return np.array([left_line, right_line])
+
+
 def canny(image):
     # Use the copy of the image as a base to generate a gray scale one
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -19,13 +61,14 @@ def display_lines(image, lines):
 
     if lines.any():
 
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
 
     return line_image
 
 # Mark the region of interest, in this case the line
+
+
 def region_of_interest(image):
     # Height of the image (y-axis), image.shape returns [y,x]
     height = image.shape[0]
@@ -55,7 +98,8 @@ lines = cv2.HoughLinesP(
     cropped_image, 2, np.pi/180, 100, np.array([]),
     minLineLength=40, maxLineGap=5)
 
-line_image = display_lines(lane_image, lines)
+averaged_lines = average_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image, averaged_lines)
 
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 
