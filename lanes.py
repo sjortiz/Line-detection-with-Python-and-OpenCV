af9968888ec6d648de40b46522dbbc99aabbd030
ShailynOrtiz <sortiz@intellisys.com.do>
2018-12-30 21:16:28 -0400

    isolating the region of interest in the canny image

--- lanes.py
+++ lanes.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def cany(image):
+def canny(image):
     # Use the copy of the image as a base to generate a gray scale one
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
@@ -29,15 +29,15 @@ def region_of_interest(image):
     # Create a polygon (shape) in the mask matrix, white filled
     cv2.fillPoly(mask, polygons, 255)
 
-    return mask
+    return cv2.bitwise_and(image, mask)
 
 
 # Read the image
 image = cv2.imread('test_image.jpg')
 # Make a copy of the image to no affect the original
-lane_image = np.copy(cany(image))
+canny_image = canny(np.copy(image))
 
 # Show the image
-cv2.imshow('result', region_of_interest(lane_image))
+cv2.imshow('result', region_of_interest(canny_image))
 # Forever
 cv2.waitKey(0)
